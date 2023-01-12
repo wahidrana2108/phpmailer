7b1669b1d17b84ebddaeefff7b020792875977fe
Wahid Rana <wahidzamanrana@gmail.com>
2023-01-13 01:03:44 +0600

    reset

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try{
-        $con =  new PDO("mysql:host=localhost;dbname=testdb", 'root', '');
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-        echo "Connection Failed: " . $e->getMessage();
-    }
-?>
